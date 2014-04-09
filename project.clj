347f049f9aee4f8401cf92c91c796c20cf0f3cd8
Jennifer Warrender <jaydchan@gmail.com>
2014-04-09 14:54:56 +0100

    upgraded to tawny-owl 1.1.0 SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject ncl.sio "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [uk.org.russet/tawny-owl "1.0-SNAPSHOT"]
-                 ]
-
+  :description "Recasting of SIO using Tawny-OWL"
+  :dependencies [[uk.org.russet/tawny-owl "1.1.0-SNAPSHOT"]]
+  :scm {:url "https://github.com/jaydchan/tawny-sio.git"
+        :name "git"}
+  :license {:name "LGPL"
+            :url "http://www.gnu.org/licenses/lgpl-3.0.txt"
+            :distribution :repo}
   :main ncl.sio.core
+  :aot [ncl.sio.core]
 )
