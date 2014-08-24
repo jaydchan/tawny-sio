736d4074df90bc18fb194d6fced47729306d55d1
Jennifer Warrender <jaydchan@gmail.com>
2014-08-24 03:35:18 +0100

    tawny update

--- project.clj
+++ project.clj
@@ -1,11 +1,26 @@
+;; The contents of this file are subject to the LGPL License, Version 3.0.
+
+;; Copyright (C) 2013-2014 Newcastle University
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see http://www.gnu.org/licenses/.
+
 (defproject ncl.sio "1.0.0-SNAPSHOT"
   :description "Recasting of SIO using Tawny-OWL"
-  :dependencies [[uk.org.russet/tawny-owl "1.1.0-SNAPSHOT"]]
+  :dependencies [[uk.org.russet/tawny-owl "1.1.1-SNAPSHOT"]]
   :scm {:url "https://github.com/jaydchan/tawny-sio.git"
         :name "git"}
   :license {:name "LGPL"
             :url "http://www.gnu.org/licenses/lgpl-3.0.txt"
             :distribution :repo}
   :main ncl.sio.core
-  :aot [ncl.sio.core]
-)
+  :aot [ncl.sio.core])
