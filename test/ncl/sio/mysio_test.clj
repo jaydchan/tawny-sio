3e4cecfd47fff7ae5ffd9da834e49d7c2ac3eb56
Jennifer Warrender <jaydchan@gmail.com>
2014-08-24 04:08:53 +0100

    include empty mysio

--- test/ncl/sio/mysio_test.clj
+++ test/ncl/sio/mysio_test.clj
@@ -1,6 +1,6 @@
 ;; The contents of this file are subject to the LGPL License, Version 3.0.
 
-;; Copyright (C) 2013, Newcastle University
+;; Copyright (C) 2013-2014, Newcastle University
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -13,19 +13,21 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see http://www.gnu.org/licenses/.
+;; along with this program. If not, see http://www.gnu.org/licenses/.
 
 (ns ncl.sio.mysio_test
   (:use [clojure.test])
   (:require
+   [tawny.owl :only [ontology-to-namespace]]
+   [tawny.read :only [iri-starts-with-filter]]
+   [tawny.reasoner :as r]
    [ncl.sio.mysio :as m]
-   [ncl.sio.sio :as s]
-   [tawny.owl :as o]
-   [tawny.reasoner :as r]))
+   [ncl.sio.rendered_sio :as rsio])
+  (:import (org.semanticweb.owlapi.model OWLOntology)))
 
 (defn ontology-reasoner-fixture [tests]
   (r/reasoner-factory :hermit)
-  (o/ontology-to-namespace m/mysio)
+  (tawny.owl/ontology-to-namespace rsio/rendered_sio)
   (binding [r/*reasoner-progress-monitor*
             (atom
             r/reasoner-progress-monitor-silent)]
@@ -39,36 +41,87 @@
   (is (r/consistent?))
   (is (r/coherent?)))
 
-(deftest ontologies
+(deftest ontology
   (is
-   (instance? org.semanticweb.owlapi.model.OWLOntology s/sio))
-  (is
-   (instance? org.semanticweb.owlapi.model.OWLOntology m/mysio)))
+   (instance? OWLOntology rsio/rendered_sio)))
 
-(deftest signature
-  (is
-   (= (count (.getSignature s/sio))
-      ;; tawny adds an annotation property
-      (- (count (.getSignature m/mysio)) 1))))
+;; (deftest signature
+;;   (is
+;;    (= (count (.getSignature rsio/rendered_sio))
+;;       (count (.getSignature m/mysio))))
+;;   (is
+;;    (= (count (filter
+;;               #(tawny.read/iri-starts-with-filter
+;;                  "http://ncl.ac.uk/sio/rendered_sio" %)
+;;               (.getSignature rsio/rendered_sio)))
+;;       (count (filter
+;;               #(tawny.read/iri-starts-with-filter
+;;                  "http://ncl.ac.uk/sio/mysio" %)
+;;               (.getSignature m/mysio))))))
 
+;; (deftest classes
+;;   (is
+;;    (= (count (.getClassesInSignature rsio/rendered_sio))
+;;       (count (.getClassesInSignature m/mysio)))))
 
-(deftest classes
-  (is
-   (= (count (.getClassesInSignature s/sio))
-      (count (.getClassesInSignature m/mysio)))))
+;; (deftest oproperties
+;;   (is
+;;    (= (count (.getObjectPropertiesInSignature rsio/rendered_sio))
+;;       (count (.getObjectPropertiesInSignature m/mysio)))))
 
-(deftest object
-   (is
-    (= (count (.getObjectPropertiesInSignature s/sio))
-       (count (.getObjectPropertiesInSignature m/mysio)))))
+;; (deftest aproperties
+;;   (is
+;;    (= (count (.getAnnotationPropertiesInSignature rsio/rendered_sio))
+;;       (count (.getAnnotationPropertiesInSignature m/mysio))))
+;;   (is
+;;    (= (count (filter
+;;               #(tawny.read/iri-starts-with-filter
+;;                  "http://ncl.ac.uk/sio/rendered_sio" %)
+;;               (.getAnnotationPropertiesInSignature rsio/rendered_sio)))
+;;       (count (filter
+;;               #(tawny.read/iri-starts-with-filter
+;;                  "http://semanticscience.org/resource/" %)
+;;               (.getAnnotationPropertiesInSignature m/mysio))))))
 
-(deftest annotation
-   (is
-    (= (count (.getAnnotationPropertiesInSignature s/sio))
-      ;; tawny adds an annotation property
-       (- (count (.getAnnotationPropertiesInSignature m/mysio)) 1))))
+;; (deftest dproperties
+;;   (is
+;;    (= (count (.getDataPropertiesInSignature rsio/rendered_sio))
+;;       (count (.getDataPropertiesInSignature m/mysio)))))
 
-(deftest data
-   (is
-    (= (count (.getDataPropertiesInSignature s/sio))
-       (count (.getDataPropertiesInSignature m/mysio)))))
+;; (deftest axioms
+;;   (is
+;;    (= (count (.getAxioms rsio/rendered_sio))
+;;       (count (.getAxioms m/mysio)))))
+
+;; (deftest subclassofaxioms
+;;   (is
+;;    (= (count
+;;        (filter
+;;         #(instance? OWLSubClassOfAxiom %)
+;;         (.getAxioms rsio/rendered_sio)))
+;;       (count
+;;        (filter
+;;         #(instance? OWLSubClassOfAxiom %)
+;;         (.getAxioms m/mysio))))))
+
+;; (deftest equivalentaxioms
+;;   (is
+;;    (= (count
+;;        (filter
+;;         #(instance? OWLEquivalentClassesAxiom %)
+;;         (.getAxioms rsio/rendered_sio)))
+;;       (count
+;;        (filter
+;;         #(instance? OWLEquivalentClassesAxiom %)
+;;         (.getAxioms m/mysio))))))
+
+;; (deftest subpropertyofaxioms
+;;   (is
+;;    (= (count
+;;        (filter
+;;         #(instance? OWLSubObjectPropertyOfAxiom %)
+;;         (.getAxioms rsio/rendered_sio)))
+;;       (count
+;;        (filter
+;;         #(instance? OWLSubObjectPropertyOfAxiom %)
+;;         (.getAxioms m/mysio))))))
