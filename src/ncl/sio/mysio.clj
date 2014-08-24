efda0814f70fa79a51eb1d967cb0f657b1525c7a
Jennifer Warrender <jaydchan@gmail.com>
2014-08-24 03:43:30 +0100

    mysio update

--- src/ncl/sio/mysio.clj
+++ src/ncl/sio/mysio.clj
@@ -1,10 +1,27 @@
+;; The contents of this file are subject to the LGPL License, Version 3.0.
+
+;; Copyright (C) 2013-2014, Newcastle University
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see http://www.gnu.org/licenses/.
+
 (ns ncl.sio.mysio
-  (:refer-clojure :only [defn if-not nil? println let partial])
-  (:use [tawny.owl])
-  (:require [tawny.read]))
+  (:refer-clojure :only [])
+  (:use [tawny.owl]))
 
 (defontology mysio
   :iri "http://ncl.ac.uk/sio/mysio"
   :prefix "mysio:")
 
- (clojure.core/load-file "./src/ncl/sio/sio_ii.clj")
+;; atoms
+(clojure.core/load-file "./src.ncl/sio/atom.clj")
