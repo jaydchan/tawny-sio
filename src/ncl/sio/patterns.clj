5dbbfce486e5dbbd467bc2ad8aee77e28e36d3b7
Jennifer Warrender <jaydchan@gmail.com>
2014-10-07 00:49:41 +0100

    updated so that entity patterns no longer use intern-owl-string function but defentity macro.

--- src/ncl/sio/patterns.clj
+++ src/ncl/sio/patterns.clj
@@ -16,20 +16,9 @@
 ;; along with this program. If not, see http://www.gnu.org/licenses/.
 
 (ns ncl.sio.patterns
-  (:use [tawny.owl :exclude [defentity]])
+  (:use [tawny.owl :exclude [defentity see-also]])
   (:require [ncl.sio.generic
              :as g :only [specific-replaces]]))
-
-;; NOTE redefined as entity-generator is private
-(defmacro ^{:private true
-            :author "Phillip Lord"} defentity
-  "Defines a new entity macro."
-  [name docstring entity-function]
-  `(defmacro
-     ~name
-     ~docstring
-     [entity# & frames#]
-     (#'tawny.owl/entity-generator entity# frames# ~entity-function)))
 
 ;; Required: while defclass utilises this, owl-class does not
 (defn make-safe
@@ -49,13 +38,13 @@ annotations."
   [o description]
   (annotation o dc-description (literal description :lang "en")))
 
-(defn sadi0
+(defn sadi
   "Annotation pattern to ensure the consistency of subset
 annotations."
   [o subset]
   (annotation o subset (literal "sadi" :lang "en")))
 
-(defn subset-rdf0
+(defn subset-rdf
   "Annotation pattern to ensure the consistency of subset
 annotations."
   [o subset value]
@@ -63,14 +52,14 @@ annotations."
 
 ;; "Annotation pattern to ensure the consistency of hasSynonym
 ;; annotations."
-(defn synonym-rdf0
+(defn synonym-rdf
   [o synonym value]
   (annotation o synonym (literal value :type :RDF_PLAIN_LITERAL)))
-(defn synonym-en0
+(defn synonym-en
   [o synonym value]
   (annotation o synonym (literal value :lang "en")))
 
-(defn example0
+(defn example
   "Annotation pattern to ensure the consistency of example
 annotations."
   [o example value]
@@ -78,16 +67,29 @@ annotations."
 
 ;; "Annotation pattern to ensure the consistency of equivalentTo
 ;; annotations."
-(defn equivalent-rdf0
+(defn equivalent-rdf
   [o equivalent value]
   (annotation equivalent (literal value :type :RDF_PLAIN_LITERAL)))
-(defn equivalent-uri0
+(defn equivalent-uri
   [o equivalent value]
   (annotation equivalent (literal value :type :XSD_ANY_URI)))
 
-(defn similar-uri0
+(defn similar-uri
   [o similar value]
   (annotation similar (literal value :type :XSD_ANY_URI)))
+
+(defn see-also-uri
+  [o value]
+  (annotation
+   o (iri "http://www.w3.org/2000/01/rdf-schema#seeAlso")
+   (literal value :type :XSD_ANY_URI)))
+
+(defn see-also
+  "Annotation pattern to ensure the consistency of see-also
+annotations."
+  [o see-also value]
+  (annotation o see-also
+              (literal value :type :RDF_PLAIN_LITERAL)))
 
 ;; see-also-rdf (using rdf:seeAlso) 1
 ;; broaderThan 1
@@ -96,86 +98,92 @@ annotations."
 ;; similar-rdf 1
 ;; subset-en 2 same value
 
-;; OPROPERTY PATTERN
-(defn sio-oproperty0
-  "Generic pattern for most sio oproperties.
-0) Interns entity
-1) Ensures name is 'safe'
-2) Ensures each property has description annotation
-3) Automatically generates the label annotation"
-  [o name description & frames]
-  (let [n (make-safe name)]
-    (intern-owl-string
-     n
-     (apply object-property
-          (list* o n
-                 :label name
-                 :annotation (desc o description)
-                 frames)))))
+(defonce ^{:private true
+           :doc "sio:see-also IRI"}
+  sio-see-also (iri "http://ncl.ac.uk/sio/mysio#seeAlso"))
 
-;; CLASS PATTERNS
-(defn sio-class0
+(defonce ^{:private true
+           :doc "sio:atom IRI"}
+  sio-atom-class (iri "http://ncl.ac.uk/sio/mysio#atom"))
+
+;; NOTE redefined as entity-generator is private
+(defmacro ^{:private true
+            :author "Phillip Lord"} defentity
+  "Defines a new entity macro."
+  [name docstring entity-function]
+  `(defmacro
+     ~name
+     ~docstring
+     [entity# & frames#]
+     (#'tawny.owl/entity-generator entity# frames# ~entity-function)))
+
+(defn make-label
+  "Takes Symbol and tries to generate label value."
+  [s]
+  (let [r (clojure.string/replace s #"_" " ")
+        ^Character f (first r)]
+    (if (Character/isWhitespace f)
+      (subs (str r) 1)
+      r)))
+
+(defn sio-class
   "Generic pattern for most sio classes.
-0) Interns entity
-1) Ensures name is 'safe'
-2) Ensures each class has description annotation
-3) Automatically generates the label annotation"
-  [o name description & frames]
-  (let [n (make-safe name)]
-    (intern-owl-string
-     n
-     (apply owl-class
-          (list* o n
-                 :label name
-                 :annotation (desc o description)
-                 frames)))))
+1) Ensures each class has description annotation
+2) Automatically generates the label annotation"
+  [name description & frames]
+  (let [o (get-current-ontology)]
+    (apply owl-class
+           o name
+           :label (make-label name)
+           :annotation (desc o description)
+           frames)))
 
-;; (defentity defsioclass
-;;     "Macro thats uses the sio-class0 pattern."
-;;   sio-class0)
-
-(defn see-also-uri0
-  [o value]
-  (annotation
-   o (iri "http://www.w3.org/2000/01/rdf-schema#seeAlso")
-   (literal value :type :XSD_ANY_URI)))
-
-(defn see-also0
-  "Annotation pattern to ensure the consistency of see-also
-annotations."
-  [o see-also value]
-  (annotation o see-also
-              (literal value :type :RDF_PLAIN_LITERAL)))
+(defentity defsclass
+  "Macro thats uses the sio-class pattern."
+  sio-class)
 
 ;; uses sio:seeAlso not rdfs:seeAlso
 (defn sio-atom-annotation-maybe
   "Pattern -- If a chebi value is provided then an annotation axiom is
 added to the atom class."
-  [o see-also cls chebi]
+  [o cls chebi]
   (if-not (nil? chebi)
     (add-annotation
      o cls
-     (see-also0 o see-also chebi)))
+     (see-also o sio-see-also chebi)))
   cls)
 
-(defn sio-atom0
+(defn sio-atom
   "Localised pattern for sio atoms.
-1) Ensures name is 'safe'
-2) Ensures each class has SubClassOf atom restriction
-3) Automatically generates the label annotation
-4) (if provided) Automatically generates the see-also annotation"
-  [o see-also atom name chebi]
-  (let [n (make-safe name)]
-    (intern-owl-string
-     n
-     (sio-atom-annotation-maybe
-      o see-also
-      (owl-class o (make-safe name)
-                 :subclass atom
-                 :label name)
-      chebi))))
+1) Ensures each class has SubClassOf atom restriction
+2) Automatically generates the label annotation
+3) (if provided) Automatically generates the see-also annotation"
+  [name chebi]
+  (let [o (get-current-ontology)]
+    (sio-atom-annotation-maybe
+     o (owl-class o name
+                  :subclass sio-atom-class
+                  :label (make-label name))
+     chebi)))
 
-;; (defentity
-;;   defsioatom
-;;   "Macro thats uses the sio-atom0 pattern."
-;;   sio-atom0)
+(defentity
+  defsatom
+  "Macro thats uses the sio-atom pattern."
+  sio-atom)
+
+(defn sio-oproperty
+  "Generic pattern for most sio oproperties.
+1) Ensures each property has description annotation
+2) Automatically generates the label annotation"
+  [name description & frames]
+  (let [o (get-current-ontology)]
+    (apply object-property
+           o name
+           :label (make-label name)
+           :annotation (desc o description)
+           frames)))
+
+(defentity
+  defsoproperty
+  "Macro thats uses the sio-oproperty pattern."
+  sio-oproperty)
